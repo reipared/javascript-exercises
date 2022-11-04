9774577deda3a6486b95323b25af8d8358fb9583
Reinaldo <reinaldopared@gmail.com>
2022-11-04 10:01:07 -0400

    Finished some exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,5 @@
 const repeatString = function() {
-
+    return "hey".repeat(3);
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
